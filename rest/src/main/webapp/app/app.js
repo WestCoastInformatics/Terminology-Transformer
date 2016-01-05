4f32c145380fc31398209910dd5fbaa6b006a301
granvold <pgranvold@westcoastinformatics.com>
2016-01-05 08:56:41 -0800

    Upload UI framework, re-added web.xml

--- rest/src/main/webapp/app/app.js
+++ rest/src/main/webapp/app/app.js
@@ -1,103 +1,107 @@
 'use strict'
 
-var ttApp = angular.module('ttApp',
-		[ 'ngRoute', 'ui.bootstrap', 'ngCookies', 'angularFileUpload' ])
-		.config(function($rootScopeProvider) {
+var ttApp = angular
+  .module('ttApp', [ 'ngRoute', 'ui.bootstrap', 'ngCookies', 'angularFileUpload' ]).config(
+    function($rootScopeProvider) {
 
-			// Set recursive digest limit higher to handle very deep trees.
-			$rootScopeProvider.digestTtl(17);
-		});
+      // Set recursive digest limit higher to handle very deep trees.
+      $rootScopeProvider.digestTtl(17);
+    });
 
 // Declare top level URL vars
 var securityUrl = 'security/';
 
 // Initialization of ttApp
-ttApp.run([ '$rootScope', '$http', '$window',
-		function($rootScope, $http, $window) {
+ttApp.run([ '$rootScope', '$http', '$window', 'securityService',
+            function($rootScope, $http, $window, securityService) {
 
-			// TODO Re-enable formal login
-			$http({
-				url : securityUrl + 'authenticate/guest',
-				method : 'POST',
-				data : 'guest',
-				headers : {
-					'Content-Type' : 'text/plain'
-				}
-			})
+  // TODO Re-enable formal login
+  $http({
+    url : securityUrl + 'authenticate/guest',
+    method : 'POST',
+    data : 'guest',
+    headers : {
+      'Content-Type' : 'text/plain'
+    }
+  }).then(function(response) {
+    securityService.setUser(response.data);
+    // set request header authorization and reroute
+    console.debug('authToken = ' + response.data.authToken);
+    $http.defaults.headers.common.Authorization = response.data.authToken;
+  });
 
-		} ]);
+} ]);
 
 // Route provider configuration
-ttApp.config([ '$routeProvider', '$logProvider',
-		function($routeProvider, $logProvider) {
-			console.debug('configure $routeProvider');
-			$logProvider.debugEnabled(true);
+ttApp.config([ '$routeProvider', '$logProvider', function($routeProvider, $logProvider) {
+  console.debug('configure $routeProvider');
+  $logProvider.debugEnabled(true);
 
-			// Set reloadOnSearch so that $location.hash() calls do not reload
-			// the
-			// controller
-			$routeProvider
+  // Set reloadOnSearch so that $location.hash() calls do not reload
+  // the
+  // controller
+  $routeProvider
 
-			.when('/', {
-				templateUrl : 'app/page/upload/upload.html',
-				controller : 'SourceDataUploadCtrl',
-				reloadOnSearch : false
-			})
+  .when('/', {
+    templateUrl : 'app/page/upload/upload.html',
+    controller : 'SourceDataUploadCtrl',
+    reloadOnSearch : false
+  })
 
-			// $locationProvider.html5Mode(true);
+  .when('/upload', {
+    templateUrl : 'app/page/upload/upload.html',
+    controller : 'SourceDataUploadCtrl',
+    reloadOnSearch : false
+  })
 
-		} ]);
+  // $locationProvider.html5Mode(true);
+
+} ]);
 
 // Header controller
-ttApp.controller('HeaderCtrl', [
-		'$scope',
-		'$location',
-		'$http',
-		'securityService',
-		function($scope, $location, $http, securityService) {
-			console.debug('configure HeaderCtrl');
+ttApp.controller('HeaderCtrl', [ '$scope', '$location', '$http', 'securityService',
+  function($scope, $location, $http, securityService) {
+    console.debug('configure HeaderCtrl');
 
-			// Declare user
-			$scope.user = securityService.getUser();
+    // Declare user
+    $scope.user = securityService.getUser();
 
-			// Logout method
-			$scope.logout = function() {
-				securityService.logout();
-			}
+    // Logout method
+    $scope.logout = function() {
+      securityService.logout();
+    }
 
-			// Open help page dynamically
-			$scope.goToHelp = function() {
-				var path = $location.path();
-				path = '/help' + path + '?authToken='
-						+ $http.defaults.headers.common.Authorization;
-				var currentUrl = window.location.href;
-				var baseUrl = currentUrl.substring(0,
-						currentUrl.indexOf('#') + 1);
-				var newUrl = baseUrl + path;
-				var myWindow = window.open(newUrl, 'helpWindow');
-				myWindow.focus();
-			};
+    // Open help page dynamically
+    $scope.goToHelp = function() {
+      var path = $location.path();
+      path = '/help' + path + '?authToken=' + $http.defaults.headers.common.Authorization;
+      var currentUrl = window.location.href;
+      var baseUrl = currentUrl.substring(0, currentUrl.indexOf('#') + 1);
+      var newUrl = baseUrl + path;
+      var myWindow = window.open(newUrl, 'helpWindow');
+      myWindow.focus();
+    };
 
-			// for ng-show
-			$scope.isShowing = function() {
-				return securityService.isLoggedIn();
-			}
+    // for ng-show
+    $scope.isShowing = function() {
+      return securityService.isLoggedIn();
+    }
 
-		} ]);
+  } ]);
 
 // Footer controller
 ttApp.controller('FooterCtrl', [ '$scope', 'gpService', 'securityService',
-		function($scope, gpService, securityService) {
-			console.debug('configure FooterCtrl');
-			// Declare user
-			$scope.user = securityService.getUser();
+  function($scope, gpService, securityService) {
+    console.debug('configure FooterCtrl');
+    // Declare user
+    $scope.user = securityService.getUser();
 
-			// Logout method
-			$scope.logout = securityService.logout;
+    // Logout method
+    $scope.logout = securityService.logout;
 
-			// for ng-show
-			$scope.isShowing = function() {
-				return securityService.isLoggedIn();
-			}
+    // for ng-show
+    $scope.isShowing = function() {
+      return securityService.isLoggedIn();
+    }
 
-		} ]);
+  } ]);
