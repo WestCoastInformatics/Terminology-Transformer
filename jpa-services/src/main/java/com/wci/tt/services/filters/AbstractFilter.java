a715e8c6134fa9d1374fa3bb844259703a77572f
Jesse Efron <yishai_il@yahoo.com>
2016-03-04 10:59:21 -0800

    Fix call to addFilteredResult() from checkFilterType() to ensure that the category is passed in for the proper creation of the filter's filename

--- jpa-services/src/main/java/com/wci/tt/services/filters/AbstractFilter.java
+++ jpa-services/src/main/java/com/wci/tt/services/filters/AbstractFilter.java
@@ -137,15 +137,18 @@ public abstract class AbstractFilter {
   protected boolean checkFilterType(String type, String inputStr)
     throws Exception {
     boolean filterRequired = false;
+    String category = null;
 
     if (configMap.get(type).containsKey(inputStr.toLowerCase())) {
       // Exact match found
       filterRequired = true;
+      category = configMap.get(type).get(inputStr);
     } else {
       // Look for keyword matches
       for (String item : configMap.get(type).keySet()) {
         if (inputStr.toLowerCase().contains(item.toLowerCase())) {
           filterRequired = true;
+          category = configMap.get(type).get(item);
           break;
         }
       }
@@ -153,7 +156,7 @@ public abstract class AbstractFilter {
 
     if (filterRequired) {
       // Must be filtered, add to set of filtered results
-      addFilteredResult(configMap.get(type).get(inputStr), inputStr);
+      addFilteredResult(category, inputStr);
     }
 
     return filterRequired;
