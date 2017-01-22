6f6a57fa56a7e08e41bbdf392f0cdf44ba9788dc
Brian Carlsen <bcarlsen@westcoastinformatics.com>
2017-01-21 18:31:22 -0800

    Clean up config stuff.

--- rest/src/main/webapp/app/appConfig.js
+++ rest/src/main/webapp/app/appConfig.js
@@ -4,23 +4,7 @@
 // NOTE:  If additional properties added, update term-server-rest/pom.xml dev-windows defaults
 tsApp.constant('appConfig', {
 
-  // deployment display variables
-  deployLink : '${deploy.link}',
-  deployTitle : '${deploy.title}',
-  deployPasswordReset : '${deploy.password.reset}',
-  deployPresentedBy : '${deploy.presented.by}',
-  deployCopyright : '${deploy.footer.copyright}',
-  deployFeedbackEmail : '${deploy.feedback.email}',
-
   // project variables
   projectVersion : '${project.version}',
-
-  // routing variables
-  landingEnabled : '${deploy.landing.enabled}',
-  licenseEnabled : '${deploy.license.enabled}',
-  loginEnabled : '${deploy.login.enabled}',
-
-  // other
-  siteTrackingCode : '${site.tracking.code}'
 
 });
