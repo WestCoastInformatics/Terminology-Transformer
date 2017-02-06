c27187c0866ea35322ed6e8bb925c40bede10b70
Patrick Granvold <granvold@gmail.com>
2017-02-06 12:14:45 -0800

    Cleanup

--- jpa-services/src/main/java/com/wci/tt/jpa/services/validation/ConceptTerminologyIdUnique.java
+++ jpa-services/src/main/java/com/wci/tt/jpa/services/validation/ConceptTerminologyIdUnique.java
@@ -11,7 +11,6 @@ import com.wci.umls.server.jpa.ValidationResultJpa;
 import com.wci.umls.server.jpa.services.ContentServiceJpa;
 import com.wci.umls.server.jpa.services.validation.AbstractValidationCheck;
 import com.wci.umls.server.model.content.Concept;
-import com.wci.umls.server.model.content.SemanticTypeComponent;
 import com.wci.umls.server.services.ContentService;
 
 /**
@@ -30,6 +29,11 @@ public class ConceptTerminologyIdUnique extends AbstractValidationCheck {
   public ValidationResult validate(Concept c) {
     ValidationResult result = new ValidationResultJpa();
     ContentService contentService = null;
+    
+    if (c.getTerminologyId() == null || c.getTerminologyId().isEmpty()) {
+      result.getErrors().add("Terminology id null or empty");
+      return result;
+    }
     try {
       contentService = new ContentServiceJpa();
 
@@ -49,46 +53,6 @@ public class ConceptTerminologyIdUnique extends AbstractValidationCheck {
         }
       }
 
-      // check name -- exact match across features
-      concepts = contentService.findConcepts(c.getTerminology(), c.getVersion(),
-          c.getBranch(),
-          "NOT id:" + c.getId() + " AND name:\"" + c.getName() + "\"", null);
-
-      for (Concept concept : concepts.getObjects()) {
-        boolean featureMatch = false;
-        for (SemanticTypeComponent sty1 : c.getSemanticTypes()) {
-          for (SemanticTypeComponent sty2 : concept.getSemanticTypes()) {
-            if (sty1.getSemanticType().equals(sty2.getSemanticType())) {
-              featureMatch = true;
-            }
-          }
-        }
-        if (!featureMatch) {
-        result.addError(
-            "Concept name not unique across features, duplicated on concept "
-                + concept.getTerminologyId());
-        }
-
-      }
-
-      // check name -- exact match within feature
-      concepts = contentService.findConcepts(null, null, c.getBranch(),
-          "NOT id:" + c.getId() + " AND name:\"" + c.getName() + "\""
-              + " AND NOT (terminology:\"" + c.getTerminology() + "\""
-              + " AND version:\"" + c.getVersion() + "\")",
-          null);
-      for (Concept concept : concepts.getObjects()) {
-        for (SemanticTypeComponent sty1 : c.getSemanticTypes()) {
-          for (SemanticTypeComponent sty2 : concept.getSemanticTypes()) {
-            if (sty1.getSemanticType().equals(sty2.getSemanticType())) {
-              result.addError(
-                  "Concept name not unique within feature " + sty2 + ", duplicated on concept "
-                      + concept.getTerminologyId());
-            }
-          }
-        }
-      
-      }
     } catch (Exception e) {
       // do nothing
     } finally {
